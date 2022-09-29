b6f8a54bd37dd58b83c0af376092587c58124a5d
ekrxjwl486 <ekrxjwl456@gmail.com>
2022-09-29 23:48:48 +0900

    board 220929_2348

---
+++ src/main/java/com/prj01/board/service/BoardService.java
@@ -0,0 +1,20 @@
+package com.prj01.board.service;
+
+
+import com.prj01.board.domain.repository.BoardRepository;
+import com.prj01.board.dto.BoardDto;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@AllArgsConstructor
+@Service
+public class BoardService {
+    private BoardRepository boardRepository;
+
+    @Transactional
+    public Long savePost(BoardDto boardDto) {
+        return boardRepository.save(boardDto.toEntity()).getId();
+    }
+}
